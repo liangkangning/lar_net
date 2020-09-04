86fa547e5b596ca332db558e4f829e35b5b5dc6f
mac <635442166@qq.com>
2020-09-04 09:23:07 +0800

    样式修改

--- frontend/views/column/connectingLithium.php
+++ frontend/views/column/connectingLithium.php
@@ -639,19 +639,19 @@ $this->registerJsFile('@web/assets/js/index.js',['depends'=>['frontend\assets\Co
                     </p>
                     <div class="img-list section60">
                         <ul class="ul_li_padding ul_none">
-                            <li class="col-md-4 common_p">
+                            <li class="col-md-4">
                                 <div class="item">
                                     <div class="img text-center"><img src="/static/images/column_connecting_p3.jpg" alt="First Parallel and Then Series of Power Battery Module Topological Structure" title="First Parallel and Then Series of Power Battery Module Topological Structure"></div>
                                     <div class="text section30 text-center">First Parallel and Then Series of Power Battery Module Topological Structure</div>
                                 </div>
                             </li>
-                            <li class="col-md-4 m_section60 common_p">
+                            <li class="col-md-4 m_section60">
                                 <div class="item">
                                     <div class="img text-center"><img src="/static/images/column_connecting_p4.jpg" alt="First Series and Then Parallel of Power Battery Module Topological Structure" title="First Series and Then Parallel of Power Battery Module Topological Structure"></div>
                                     <div class="text section30 text-center">First Series and Then Parallel of Power Battery Module Topological Structure</div>
                                 </div>
                             </li>
-                            <li class="col-md-4 m_section60 common_p">
+                            <li class="col-md-4 m_section60">
                                 <div class="item">
                                     <div class="img text-center"><img src="/static/images/column_connecting_p5.jpg" alt="First Parallel, Then Series and Parallel Again of Power Battery Module Topological Structure" title="First Parallel, Then Series and Parallel Again of Power Battery Module Topological Structure"></div>
                                     <div class="text section30 text-center">First Parallel, Then Series and Parallel Again of Power Battery Module Topological Structure</div>
