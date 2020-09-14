110b171633580ef6b9d8abe9287750163480bc4b
mac <635442166@qq.com>
2020-09-14 17:16:15 +0800

    样式修改

--- frontend/views/layouts/public/header_old.php
+++ frontend/views/layouts/public/header_old.php
@@ -36,7 +36,7 @@
                     <div class="t2">
                         <div class="dropdown">
                             <div class="ico"><img src="<?=Yii::getAlias('@web/static/images/global.png')?>" alt=""></div>
-                            <a id="dLabel" data-target="#" href="http://www.juda.cn">
+                            <a id="dLabel" data-target="#" target="_blank" href="http://www.juda.cn">
                                 简体中文
                                 <span class="caret"></span>
                             </a>
