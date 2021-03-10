b6c52f1b6e96708cad2b314683e2cd29e5434810
梁康宁 <635442166@qq.com>
2021-03-10 14:19:57 +0800

    修改邮箱密码

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -55,7 +55,7 @@ return [
                 'class' => 'Swift_SmtpTransport',
                 'host' => 'smtp.exmail.qq.com',
                 'username' => 'liangkangning@juda.cn',
-                'password' => 'aVZJdwKDAmwpWB23',
+                'password' => 'VZJdwKDAmwpWB23',
                 'port' => '465',
                 'encryption' => 'ssl',
             ],
